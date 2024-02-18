8577a8d41a1a2b987569e1a8309a5606ef36ded9
Brayden Anderson <braydenanderson2014@gmail.com>
2024-02-18 15:50:12 -0700

    Upload of all files for Repository

---
+++ README.md
@@ -0,0 +1,125 @@
+# DoubleLinkedList for Arduino
+
+A simple, templated linked list implementation for Arduino projects. This class allows you to create and manage a linked list of any data type.
+
+## Features
+
+- Templated class: Can store any data type.
+- Dynamic resizing: Automatically manages memory as elements are added or removed.
+- Basic operations: `append`, `prepend`, `insert`, `remove`, `get`, `contains`, `getSize`, `isEmpty`, `clear`.
+
+## WARNING: This Library Utilizes POINTERS *. This is due to the Libraries ability to utilize any return type. (Bool, String, int, float, etc)
+
+
+## Installation
+
+To use `DoubleLinkedList` in your Arduino sketch:
+
+1. Copy the `DoubleLinkedList` class code to your project's directory.
+2. Include `DoubleLinkedList.h` at the top of your sketch.
+
+## Change Log
+### Version 1.0.0
+* Initial Release
+* Added Library name in the Serial Outputs for debugging purposes
+* This particular version is not tested and may not work as intended. USE AT YOUR OWN RISK!",
+* This Library is extremely similar to the LinkedList Library, but with a few changes to the code to make it a double linked list instead of a single linked list.
+### Version 1.0.1
+* "Added a boolean to the Constructor that determines if the Library should print the Debug Messages to the Serial Monitor or not. This is useful for when you want to use the Serial Monitor for other things and don't want the Debug Messages to interfere with the other things you are doing.
+* Added a new function called setDebugMode() that allows you to change the Debug Mode after the Library has been initialized. This is useful for when you want to use the Serial Monitor for other things and don't want the Debug Messages to interfere with the other things you are doing.
+* Added a new function called getDebugMode() that allows you to get the current Debug Mode of the Library. This is useful for when you want to use the Serial Monitor for other things and don't want the Debug Messages to interfere with the other things you are doing.
+* By default, the Debug Mode is set to false. This means that the Debug Messages will not be printed to the Serial Monitor if you do not pass the boolean in. This is useful for when you want to use the Serial Monitor for other things and don't want the Debug Messages to interfere with the other things you are doing.
+### Version 1.0.2
+* Added new getAsString() function that returns the list element as a String. This is useful for when you want to print the list to the Serial Monitor or to a LCD Screen.
+* Deprecated valueExists() function, as it is no longer needed. This function will be removed in the next release. Use the contains function instead
+* Removed Debug Messages from the Library, This is to save Memory space.
+* Added Function Comments to all of the functions in the Library
+### Version 1.0.3
+* Added getElement Function that returns the element instead of the pointer to the element.
+### Version 1.0.4
+* Renamed the remove function to more closely correlate to what it does.
+* Added new remove function that takes in an index to remove to match other List Libraries.
+            
+
+
+## Usage
+
+To create a linked list, simply declare an instance of `DoubleLinkedList` with the desired type:
+
+```cpp
+#include <DoubleLinkedList.h>
+
+DoubleLinkedList<int> myList;
+DoubleLinkedList<int> debugList(true);
+```
+## Adding Elements
+### Append an element to the end of the list:
+
+```cpp
+myList.append(1);
+```
+### Prepend an element to the beginning of the list:
+
+```cpp
+myList.prepend(0);
+```
+### Insert an element at a specific position:
+
+```cpp
+myList.insert(2, 1);  // Insert '2' at position '1'
+```
+## Accessing Elements
+### Retrieve an element at a specific position:
+
+```cpp
+int value = myList.get(1);
+```
+## Removing Elements
+### Remove an element by value:
+
+```cpp
+myList.remove(1);
+```
+## Utility Functions
+### Check if the list contains a specific value:
+
+```cpp
+if (myList.contains(2)) {
+    // Element is in the list
+}
+```
+## Get the size of the list:
+
+```cpp
+size_t size = myList.getSize();
+```
+### Check if the list is empty:
+
+```cpp
+if (myList.isEmpty()) {
+    // List is empty
+}
+```
+### Clear the list:
+
+```cpp
+myList.clear();
+```
+## Example
+```cpp
+LinkedList<int> myList;
+myList.append(1);
+myList.append(2);
+myList.prepend(0);
+myList.insert(3, 3);
+
+for (size_t i = 0; i < myList.getSize(); i++) {
+    Serial.print("Element at position ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(myList.get(i));
+}
+
+myList.remove(2);
+myList.clear();
+```
---
+++ src/DoubleLinkedList.h
@@ -0,0 +1,546 @@
+#ifndef DOUBLELINKEDLIST_H
+#define DOUBLELINKEDLIST_H
+
+#include <Arduino.h>
+
+/**
+ * @brief Double Linked List Node
+ * @param T Data type
+ * @note This class is used to create a node for the double linked list
+ * 
+ * @return Double Linked List Node
+*/
+template <typename T>
+class DoubleListNode {
+public:
+    T data;
+    DoubleListNode *next;
+    DoubleListNode *prev; // Pointer to the previous node
+    DoubleListNode(T value) : data(value), next(nullptr), prev(nullptr) {}
+};
+
+/**
+ * @brief Double Linked List
+ * @param T Data type
+ * @note This class is used to create a double linked list
+ * @details A Double Linked List is a List that has a pointer to the next and previous node
+*/
+template <typename T>
+class DoubleLinkedList {
+private:
+    DoubleListNode<T> *head; // Pointer to the first node
+    DoubleListNode<T> *tail; // Pointer to the last node
+    size_t Size;            // Number of elements in the list
+    // Random Iterator Helper
+    mutable bool *visited;       // Array to keep track of visited nodes
+    mutable size_t visitedCount; // Number of visited nodes
+
+public:
+    /**
+     * @brief Forward Iterator
+     * @param T Data type
+     * @note This class is used to create a forward iterator for the double linked list
+    */
+    class ForwardIterator {
+    private:
+        DoubleListNode<T> *current;
+
+    public:
+        /**
+         * @brief Construct a new Forward Iterator object
+        */
+        ForwardIterator(DoubleListNode<T> *start) : current(start) {}
+
+        /**
+         * @brief Destroy the Forward Iterator object
+        */
+        ~ForwardIterator(){}
+
+        /**
+         * @brief Dereference operator
+        */
+        T &operator*() {
+            return current->data;
+        }
+
+        /**
+         * @brief Not equal operator
+        */
+        bool operator!=(const ForwardIterator &other) const {
+            return current != other.current;
+        }
+
+        /**
+         * @brief Increment operator
+        */
+        ForwardIterator &operator++() {
+            if (current) current = current->next;
+            return *this;
+        }
+    };
+
+    // Backward iterator class definition
+    /**
+     * @brief Backward Iterator
+    */
+    class BackwardIterator {
+    private:
+        DoubleListNode<T> *current;
+    public:
+        /**
+         * @brief Construct a new Backward Iterator object
+        */
+        BackwardIterator(DoubleListNode<T> *start) : current(start) {}
+
+        /**
+         * @brief Destroy the Backward Iterator object
+        */
+        ~BackwardIterator() {}
+
+        /**
+         * @brief Dereference operator
+        */
+        T &operator*() {
+            return current->data;
+        }
+
+        /**
+         * @brief Not equal operator
+        */
+        bool operator!=(const BackwardIterator &other) const {
+            return current != other.current;
+        }
+
+        /**
+         * @brief Decrement operator
+        */
+        BackwardIterator &operator--() {
+            if (current) current = current->prev;
+            return *this;
+        }
+    };
+
+    // Random iterator class definition
+    /**
+     * @brief Random Iterator
+    */
+    class RandomIterator {
+    private:
+        DoubleLinkedList<T> *list;
+
+        /**
+         * @brief Reset the visited list
+         * @details This method resets the visited list
+        */
+        void resetVisited() const {
+            for (size_t i = 0; i < list->Size; ++i) {
+                list->visited[i] = false;
+            }
+            list->visitedCount = 0;
+        }
+
+    public:
+        /**
+         * @brief Construct a new Random Iterator object
+        */
+        RandomIterator(DoubleLinkedList<T> *list) : list(list) {
+            list->visited = new bool[list->Size]();
+            resetVisited();
+        }
+
+        /**
+         * @brief Destroy the Random Iterator object
+        */
+        ~RandomIterator() {
+            delete[] list->visited;
+        }
+
+        /**
+         * @brief Dereference operator
+        */
+        T &operator*() {
+            size_t index = random(0, list->Size - list->visitedCount);
+            size_t count = 0;
+            for (size_t i = 0; i < list->Size; ++i) {
+                if (!list->visited[i]) {
+                    if (count == index) {
+                        list->visited[i] = true;
+                        list->visitedCount++;
+                        return *list->get(i);
+                    }
+                    count++;
+                }
+                // Return the first unvisited element as a fallback
+                for (size_t i = 0; i < list->Size; ++i) {
+                    if (!list->visited[i]) {
+                        list->visited[i] = true;
+                        list->visitedCount++;
+                        return *list->get(i);
+                    }
+                }
+                // Should not reach here
+            }
+        }
+
+        /**
+         * @brief has next operator
+         * @details This method checks if there is a next element
+        */
+        bool hasNext() const {
+            return list->visitedCount < list->Size;
+        }
+
+        /**
+         * @brief Reset the iterator
+         * @details This method resets the iterator
+        */
+        void reset() {
+            resetVisited();
+        }
+    };
+
+    // Methods to get iterators
+    /**
+     * @brief Get the begin Forward iterator
+     * @details This method gets the begin forward iterator
+     * 
+     * @return ForwardIterator
+    */
+    ForwardIterator begin() const {
+        return ForwardIterator(head);
+    }
+
+    /**
+     * @brief Get the end Forward iterator
+     * @details This method gets the end forward iterator
+     * 
+     * @return ForwardIterator
+    */
+    ForwardIterator end() const {
+        return ForwardIterator(nullptr);
+    }
+
+    /**
+     * @brief Get the begin Backward iterator
+     * @details This method gets the begin backward iterator
+     * 
+     * @return BackwardIterator
+    */
+    BackwardIterator rbegin() const {
+        return BackwardIterator(tail);
+    }
+
+    /**
+     * @brief Get the end Backward iterator
+     * @details This method gets the end backward iterator
+     * 
+     * @return BackwardIterator
+    */
+    BackwardIterator rend() const {
+        return BackwardIterator(nullptr);
+    }
+
+    /**
+     * @brief Get the Random Iterator
+     * @details This method gets the random iterator
+     * 
+     * @return RandomIterator
+    */
+    RandomIterator randomBegin() {
+        return RandomIterator(this);
+    }
+
+    /**
+     * @brief Construct a new Double Linked List object
+     * @details This method constructs a new double linked list
+    */
+    DoubleLinkedList() : head(nullptr), tail(nullptr), Size(0) {}
+
+    /**
+     * @brief Destroy the Double Linked List object
+     * @details This method destroys the double linked list
+    */
+    ~DoubleLinkedList() {
+        clear();
+    }
+
+    /**
+     * @brief Append an element to the end of the list
+     * @details This method appends an element to the end of the list
+     * 
+     * @param value Value to append
+    */
+    void append(const T &value) {
+        DoubleListNode<T> *newNode = new DoubleListNode<T>(value);
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        Size++;
+    }
+
+    /**
+     * @brief Prepend an element to the beginning of the list
+     * @details This method prepends an element to the beginning of the list
+     * 
+     * @param value Value to prepend
+    */
+    void prepend(const T &value) {
+        DoubleListNode<T> *newNode = new DoubleListNode<T>(value);
+        newNode->next = head;
+        if (head) {
+            head->prev = newNode;
+        } else {
+            tail = newNode;
+        }
+        head = newNode;
+        Size++;
+    }
+
+    /**
+     * @brief Insert an element at a random position
+     * @details This method inserts an element at a random position
+     * 
+     * @param value Value to insert
+    */
+    void insert(const T &value) {
+        int randomNum = random(0, Size);
+        if (randomNum == 0) {
+            prepend(value);
+        } else if (randomNum >= Size) {
+            append(value);
+        } else {
+            DoubleListNode<T> *newNode = new DoubleListNode<T>(value);
+            DoubleListNode<T> *current = head;
+            for (size_t i = 1; i < randomNum; i++) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+            Size++;
+        }
+    }
+    // Insert an element at a specific position
+    /**
+     * @brief Insert an element at a specific position
+     * @details This method inserts an element at a specific position
+     * 
+     * @param value Value to insert
+     * @param position Position to insert
+     * @note If the position is 0, the element will be prepended
+     * @note If the position is greater than or equal to the size, the element will be appended
+     * @note If the position is within the size, the element will be inserted at that position
+    */
+    void insert(const T &value, size_t position) {
+        if (position == 0) {
+            prepend(value);
+        } else if (position >= Size) {
+            append(value);
+        } else {
+            DoubleListNode<T> *newNode = new DoubleListNode<T>(value);
+            DoubleListNode<T> *current = head;
+            for (size_t i = 0; i < position; i++) {
+                current = current->next;
+            }
+            newNode->next = current;
+            newNode->prev = current->prev;
+            current->prev->next = newNode;
+            current->prev = newNode;
+            Size++;
+        }
+    }
+
+    /**
+     * @brief Remove the element at a specific position
+     * @details This method removes the element at a specific position
+    */
+    void remove(const int &position) {
+        DoubleListNode<T> *current = head;
+        for (size_t i = 0; i < position; i++) {
+            current = current->next;
+        }
+        if (current->prev) {
+            current->prev->next = current->next;
+        } else {
+            head = current->next;
+        }
+        if (current->next) {
+            current->next->prev = current->prev;
+        } else {
+            tail = current->prev;
+        }
+        delete current;
+        Size--;
+    }
+
+
+    /**
+     * @brief Remove the element from the list
+     * @details This method removes the first occurrence of an element from the list
+    */
+    void removeElement(const T &value) {
+        DoubleListNode<T> *current = head;
+        while (current) {
+            if (current->data == value) {
+                if (current->prev) {
+                    current->prev->next = current->next;
+                } else {
+                    head = current->next;
+                }
+                if (current->next) {
+                    current->next->prev = current->prev;
+                } else {
+                    tail = current->prev;
+                }
+                delete current;
+                Size--;
+                return;
+            }
+            current = current->next;
+        }
+    }
+
+    // Rest of the methods...
+    // Change the return type to a pointer
+    /**
+     * @brief Get the element at a specific position
+     * @details This method gets the element at a specific position
+     * 
+     * @param position Position to get
+     * @return T* Pointer to the element
+     * @note If the position is out of bounds, nullptr will be returned
+     * @note If the position is within bounds, a pointer to the element will be returned
+     * @note If the position is within bounds, but the element is nullptr, nullptr will be returned
+     * @warning This method returns a pointer to the element, not the element itself
+    */
+    T *get(size_t position) const {
+        DoubleListNode<T> *current = head;
+        for (size_t i = 0; i < position; i++) {
+            if (!current) {
+                return nullptr; // Out of bounds
+            }
+            current = current->next;
+        }
+        if (current) {
+            return &(current->data);
+        } else {
+            return nullptr;
+        }
+    }
+
+    T getElement(size_t position) const {
+        DoubleListNode<T> *current = head;
+        for (size_t i = 0; i < position; i++) {
+            if (!current) {
+                return; // Out of bounds
+            }
+            current = current->next;
+        }
+        if (current) {
+            return current->data;
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * @brief Get the element at a specific position as a string
+     * @details This method gets the element at a specific position and returns it as a string
+     * 
+     * @param position Position to get
+     * @return String Element at the position
+     * 
+    */
+    String getAsString(size_t position) const {
+        DoubleListNode<T> *current = head;
+        for (size_t i = 0; i < position; i++) {
+            if (!current) {
+                return nullptr; // Out of bounds
+            }
+            current = current->next;
+        }
+        if (current) {
+            return String(current->data);
+        } else {
+            return nullptr;
+        }
+    }
+    // Check if the list contains a specific element
+    /**
+     * @brief Check if the list contains a specific element
+     * @details This method checks if the list contains a specific element
+     * 
+     * @param value Value to check
+     * @return true If the list contains the element
+    */
+    bool contains(const T &value) const {
+        DoubleListNode<T> *current = head;
+        while (current) {
+            if (current->data == value) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+
+    // Get the number of elements in the list
+    /**
+     * @brief Get the number of elements in the list
+     * @details This method gets the number of elements in the list
+     * 
+     * @return size_t Number of elements in the list
+    */
+    size_t size() const {
+        return Size;
+    }
+
+    // Check if the list is empty
+    /**
+     * @brief Check if the list is empty
+     * @details This method checks if the list is empty
+     * 
+     * @return true If the list is empty
+    */
+    bool isEmpty() const {
+        return Size == 0;
+    }
+
+    // Clear the list and release memory
+    /**
+     * @brief Clear the list and release memory
+     * @details This method clears the list and releases memory
+    */
+    void clear() {
+        while (head) {
+            DoubleListNode<T> *temp = head;
+            head = head->next;
+            delete temp;
+        }
+        Size = 0;
+    }
+
+    /**
+     * @brief checks if the value exists in the list
+     * @details This method checks if the value exists in the list
+     * @deprecated This method is deprecated and will be removed in the next version
+     * @see contains()
+     * 
+     * @param value Value to check
+    */
+    bool valueExists(const T &value) const {
+        DoubleListNode<T> *current = head;
+        while (current) {
+            if (current->data == value) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+};
+#endif // DOUBLELINKEDLIST_H
